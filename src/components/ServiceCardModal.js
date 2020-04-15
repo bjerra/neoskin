fff03fbdeb354cba7f9ba99d4edb112b909a3397
Bjorn <ebjarne@gmail.com>
2020-04-15 21:16:51 +0200

    added services

--- src/components/ServiceCardModal.js
+++ src/components/ServiceCardModal.js
@@ -5,13 +5,18 @@ const ServiceCardModal = ({data, close, isOpen}) => {
     
     if(!data) return null;
     return(   
-        <div className="modal is-active slider slide-in">
-            <div className="modal-background fade-in" onClick={close}>></div>
-            <div className="modal-content">
-                <ServiceCard data={data} />
-            </div>
-            <button className="modal-close is-large" aria-label="close" onClick={close}></button>
+        
+            <div className="modal is-active" style={{overflow: 'none'}}>
+                
+                        <div className="modal-background fade-in" onClick={close}>></div>
+                        <div className="modal-content slider slide-in">
+                            <ServiceCard data={data} />
+                      
+                        </div>
+                        <button className="modal-close is-large" aria-label="close" onClick={close}></button>
+                    
         </div>
+        
                                    
     )
 }
